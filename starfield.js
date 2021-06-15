6ae1b2305a5683b331bc9ca70deaef265fc24ff3
rxpha13 <79053516+rxpha13@users.noreply.github.com>
2021-06-15 17:56:38 -0300

    Update starfield.js

--- starfield.js
+++ starfield.js
@@ -114,6 +114,113 @@ start()
 	}
 
 
+  update(){
+	var dt = 1 / this.fps;
+
+	for(var i=0; i<this.stars.length; i++) {
+		var star = this.stars[i];
+		star.y += dt * star.velocity;
+		//	If the star has moved from the bottom of the screen, spawn it at the top.
+		if(star.y > this.height) {
+			var x = Math.random()*this.width;
+			var size =  Math.random()*1+1;
+			var vel = (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity;
+			let s = new Star(x, 0,size, vel);
+			//console.table(s);			
+		 	this.stars[i] = s;
+		}
+		
+	}
+	//console.table(this.stars);
+}
+
+stop()
+{
+	clearInterval(this.intervalId);
+}
+	
+}
+	console.table(window.innerWidth);
+	this.height = window.innerHeight;
+	console.table(window.innerHeight);
+	this.ctx.canvas.width = this.width;
+    this.ctx.canvas.height = this.height;
+	    
+  }
+  
+ 
+inicializa()
+{
+	
+	var self = this;
+	 
+addEventListener('resize', function resize(event){
+		//redefine as medidas quando a janela é alterada
+    
+	self.width = window.innerWidth;
+    self.height = window.innerHeight;
+	self.ctx.canvas.width = self.width;
+	self.ctx.canvas.height = self.height;
+	console.table(self.ctx.width);
+	console.table(self.ctx.height);
+    self.desenha() 
+	
+	 /*this.width = window.innerWidth;
+    this.height = window.innerHeight;
+	this.ctx.width = this.width;
+	this.ctx.height = this.height;
+	console.table(this.ctx); */
+	
+}); 
+
+}
+start()
+{
+	
+	this.stars = [];
+	//console.table(stars);
+	for(var i=0; i<this.qtdestars; i++) {
+		//console.table("start");
+		
+			var x = Math.random()*this.width;
+			
+			var y =  Math.random()*this.height;
+			var size =  Math.random()*1+1;
+			var vel = (Math.random()*(this.maxVelocity - this.minVelocity))+this.minVelocity;
+			
+			this.star = new Star(x,y, size,vel );
+			//console.table(s.x);
+			//console.table(this.star);
+		this.stars[i] = this.star;
+	}
+	
+//console.table(stars);
+	var timeUpdate = 1000 / this.fps;
+	var self = this;
+	this.intervalId = setInterval(function() {
+        self.update();
+        self.desenha();	
+    }, timeUpdate);
+}
+
+	desenha()
+	{
+		//console.table("teste");
+		//desenha o plano de fundo
+		this.ctx.fillStyle = '#000000';
+		this.ctx.fillRect(0, 0, this.width, this.height);
+		
+		//desenha as estrelas no plano de fundo
+		this.ctx.fillStyle = '#ffffff';
+		for(var i=0; i<this.stars.length;i++) {
+			this.star = this.stars[i];
+			this.ctx.fillRect(this.star.x, this.star.y, this.star.size, this.star.size);
+			//console.table(star);
+		}
+		
+	}
+
+
 update(){
 	var dt = 1 / this.fps;
 
